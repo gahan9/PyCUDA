8fc2e02ef842fb843d884d07428e61012a465e1b
gahan9 <gahansaraiya@gmail.com>
2018-08-23 23:12:39 +0530

    first cuda program to double array

---
+++ hello_gpu.py
@@ -0,0 +1,40 @@
+"""
+https://documen.tician.de/pycuda/tutorial.html
+"""
+# Import and initialize PyCuda
+from pycuda import driver
+import pycuda.driver as cuda
+from pycuda.compiler import SourceModule
+import pycuda.autoinit  # optional use >> reason: initialization, context creation, and cleanup can also be performed manually
+
+import numpy  # utilized to transfer data onto the device; transfer data from numpy arrays on the host
+
+
+a = numpy.random.randn(4, 4)
+# require conversion because variable a consists of double precision numbers,
+# but most nVidia devices only support single precision
+a = a.astype(numpy.float32)
+
+# a_gpu = cuda.mem_alloc_like(a)  # allocate memory on the device
+# cuda.memcpy_htod(a_gpu, a)  # transfer data to gpu
+a_gpu = cuda.to_device(a)  # above two lines of code clubbed in one now
+
+
+# write code to double each entry in a_gpu
+mod = SourceModule("""
+    __global__ void doublify(float *a)
+    {
+        int idx = threadIdx.x + threadIdx.y*4;
+        a[idx] *= 2;
+    }
+""")
+
+func = mod.get_function("doublify")
+func(a_gpu, block=(4, 4, 1))
+
+# fetch the data back from the GPU and display it
+# a_doubled = numpy.empty_like(a)
+# cuda.memcpy_dtoh(a_doubled, a_gpu)
+a_doubled = cuda.from_device_like(devptr=a_gpu, other_ary=a)  # above two lines of code clubbed in one now
+print(a_doubled)
+print(a)
