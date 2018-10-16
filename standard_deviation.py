596282fdc3f0131a7fd7f61cce57994ae7c75644
gahan9 <gahansaraiya@gmail.com>
2018-10-16 15:37:08 +0530

    fixed typo

--- standard_deviation.py
+++ standard_deviation.py
@@ -38,7 +38,7 @@ class Vector(object):
         self.total_elements = total_elements
         # self.a = numpy.random.randn(self.total_elements).astype(numpy.float32)
         # self.b = numpy.random.randn(self.total_elements).astype(numpy.float32)
-        self.a = numpy.array([i for i in range(10)])
+        self.a = numpy.array([i for i in range(10)]).astype(numpy.float32)
         self.result = numpy.zeros_like(self.a)
 
     def sum(self, blocks=None, threads=1):
