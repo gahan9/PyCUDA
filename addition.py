b781838b11eb258dc7d001840e2817c8d1d733d6
gahan9 <gahansaraiya@gmail.com>
2018-10-16 14:42:12 +0530

    test added

--- addition.py
+++ addition.py
@@ -23,22 +23,23 @@ __global__ void addition(float *result, float *a, float *b)
 
 
 class Vector(object):
-    def __init__(self, array_size):
+    def __init__(self, total_elements=512):
+        self.total_elements = total_elements
         self.a = numpy.random.randn(array_size).astype(numpy.float32)
         self.b = numpy.random.randn(array_size).astype(numpy.float32)
         self.result = numpy.zeros_like(self.a)
 
     def addition(self, blocks=None, threads=1):
-        blocks = blocks if blocks else len(self.a)
+        blocks = blocks if blocks else self.total_elements
         addition = mod.get_function("addition")
         addition(
             driver.Out(self.result), driver.In(self.a), driver.In(self.b),
-            block=(blocks, threads, 1), grid=(1, 1))  # block = (blocks, threads, 1)
+            block=(blocks, 1, 1), grid=(512, 512))  # block = (blocks, threads, 1)
 
 
 def test(array_size=400):
     vector_obj = Vector(array_size)
-    vector_obj.addition(blocks=512, threads=16)
+    vector_obj.addition(blocks=512, threads=1)
     print("Vector a:---\n{}".format(vector_obj.a))
     print("Vector b:---\n{}".format(vector_obj.b))
     print("Result:----\n", vector_obj.result)
@@ -48,4 +49,6 @@ if __name__ == "__main__":
     import sys
     if len(sys.argv) > 1:
         array_size = int(sys.argv[1])
-    test(array_size)
+        test(array_size)
+    else:
+        test()
